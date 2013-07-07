661234d9d9c1b8add8519b09e0232bb15075125d
Ubuntu <ubuntu@ip-10-245-226-98.ec2.internal>
2013-07-07 18:21:31 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs=require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//readFileSync returns a buffer if no encoding is specified,
+var buffer = fs.readFileSync('index.html');
+app.get('/', function(request, response) 
+{
+	//toString default to utf8. Adding to illustrate it
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
